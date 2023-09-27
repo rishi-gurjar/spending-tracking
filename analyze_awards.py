8301b691bfe5a64b2de3f0e7af07474fa750a0e1
Rishi Gurjar <rishigurjar2000@gmail.com>
2023-09-27 00:35:23 -0400

    return financial data but not via name, but ticker

--- analyze_awards.py
+++ analyze_awards.py
@@ -1,32 +1,31 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import requests
+from dotenv import load_dotenv
+import os
+import json
+
+load_dotenv()
 
 # Load the CSV file into a Pandas DataFrame
 df = pd.read_csv('awards.csv')
 
-# Convert 'Start Date' to datetime format
-df['Start Date'] = pd.to_datetime(df['Start Date'])
+def get_stock_ticker_fmp(api_key, company_name):
+#    base_url = f"https://financialmodelingprep.com/api/v3/search?query={company_name}&limit=10&exchange=NASDAQ&apikey={api_key}"
+    print("Api key is", api_key)
+    base_url = f"https://financialmodelingprep.com/api/v3/balance-sheet-statement/{company_name}?&limit=120&apikey={api_key}"
+    response = requests.get(base_url)
+    data = response.json()
+    return data
 
-# 1. Total award funding per month over time by "Start Date"
-df['Start Month'] = df['Start Date'].dt.to_period('M')  # Extract month and year
-monthly_funding = df.groupby('Start Month')['Award Amount'].sum().reset_index()
+    # Assuming the first match is the most relevant
+#    if len(data) > 0:
+#        return data
+#    else:
+#        return None
 
-plt.figure(figsize=(12, 6))
-plt.plot(monthly_funding['Start Month'].astype(str), monthly_funding['Award Amount'])
-plt.title('Total Award Funding Per Month Over Time by Start Date')
-plt.xlabel('Start Month')
-plt.ylabel('Total Award Amount')
-plt.xticks(rotation=45)
-plt.show()
+api_key = os.getenv("FMP_KEY")
+company_name = "AAPL"
 
-# 2. Award funding by month (ignoring the year)
-df['Start Month Only'] = df['Start Date'].dt.month  # Extract only the month
-monthly_funding_only = df.groupby('Start Month Only')['Award Amount'].sum().reset_index()
-
-plt.figure(figsize=(12, 6))
-plt.bar(monthly_funding_only['Start Month Only'], monthly_funding_only['Award Amount'])
-plt.title('Award Funding by Month')
-plt.xlabel('Month')
-plt.ylabel('Total Award Amount')
-plt.xticks(range(1, 13), ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
-plt.show()
+data = get_stock_ticker_fmp(api_key, company_name)
+print(json.dumps(data, indent=2))
